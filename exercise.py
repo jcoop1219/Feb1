1063039603a9a6de76fb7473eaf934da368f9fe0
Jason Cooper <jason.cooper.12@gmail.com>
2019-02-01 14:24:40 -0500

    First commit

---
+++ exercise.py
@@ -0,0 +1,17 @@
+def subtractNum(a, b):
+    c = a - b
+    return c
+
+def minMaxList(list):
+    min = None
+    max = None
+    for i in list:
+        if min = None:
+            min = i
+        elif i < min:
+            min = 1
+        if max = None:
+            max = i
+        elif i > max:
+            max = i
+    return min, max
